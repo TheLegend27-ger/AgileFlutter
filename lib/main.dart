05d3e67defd3b4192d7350dfb79635d38b5884d1
ImDuude <82956212+ImDuude@users.noreply.github.com>
2022-12-15 12:14:05 +0100

    Update main.dart
    code commentary

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tiermedizinische Datenbank',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Tiermedizinische Datenbank'),
@@ -35,14 +26,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  // This widget is the home page the application.
 
   final String title;
 
@@ -52,54 +36,45 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  List text = [{}] ;
+  List text = [{}] ; //create list
   void _incrementCounter() async {
-    var db = await mongo.Db.create("mongodb+srv://Cluster2User:gPId3Gm4a0I9icN5@agile2cluster.bxvcw4l.mongodb.net/Test?retryWrites=true&w=majority");
+    var db = await mongo.Db.create("mongodb+srv://Cluster2User:gPId3Gm4a0I9icN5@agile2cluster.bxvcw4l.mongodb.net/Test?retryWrites=true&w=majority"); //database connection string
     //mongodb+srv://Cluster2User:gPId3Gm4a0I9icN5@agile2cluster.bxvcw4l.mongodb.net/Test?retryWrites=true&w=majority
-    await db.open();
+    await db.open(); //connect to database
     var collection = db.collection('CollectionTest');
 
-    var mycol = await collection.find().toList();
+    var mycol = await collection.find().toList(); //list all database objects
     print(mycol);
     setState(()  {
       text = mycol;
       _counter++;
     });
-    await db.close();
+    await db.close(); //close connection
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: SingleChildScrollView(
           child: Column(
-            children: List.generate(text.length, (index) {
+            children: List.generate(text.length, (index) { //generate list
               return ListTile(
-                title: Text(text[index]['name'].toString()),
+                title: Text(text[index]['name'].toString()), //display list
               );
             }),
           ),
         )
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton( //refresh button
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), 
     );
   }
 }
